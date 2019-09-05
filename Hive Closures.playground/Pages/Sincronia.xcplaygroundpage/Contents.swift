89718766b10fc457dade4b1c85135a9ef254aea9
pietro1704 <pietropugliesi@icloud.com>
2019-09-05 13:44:41 -0300

    Closures e assincronia

---
+++ Hive Closures.playground/Pages/Sincronia.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+//sem @escaping
+func sincrona(lista:[Int], imprime: (Int) -> ()){
+	var soma = 0
+	for valor in lista{
+		soma += valor
+	}
+
+	imprime(soma)
+}
+
+sincrona(lista: [1, 2, 3, 4, 5]) { (somaParaImprimir) in
+	print("sincrona:")
+	print("a soma da lista é \(somaParaImprimir)")
+}
+
+
+func assincrona(lista:[Int], imprime: @escaping ((Int)->Void)){
+
+	OperationQueue().addOperation {
+		var soma = 0
+		for valor in lista{
+			soma += valor
+		}
+		OperationQueue.main.addOperation {
+			imprime(soma)
+		}
+	}
+}
+
+print("antes da função assincrona:")
+assincrona(lista: [6, 7, 8, 9]) { (somaAsync) in
+	//Função imprime
+	print("eu sou a função imprime e só executei agora...")
+	print("O valor resultante da função é é \(somaAsync)")
+	//acabou a funcao imprime
+}
+
+print("retornou da função assincrona")
+
+
+
---
+++ Hive Closures.playground/Sincronia.playground/Resources/Closures.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//passo a funcao de completion como parâmetro
+func executadoPrimeiro(completion: (Int) -> ()){
+	print("primeiro")
+	completion(2)
+}
+
+func executadoDepois(){
+	print("Acabei")
+}
+
+//na chamada de executadoPrimeiro eu defino o corpo da completion
+executadoPrimeiro { (a:Int) -> () in
+
+	print("primeira chamada, posso chamar a funcao executadoDepois(), olha só")
+	executadoDepois()
+
+	print(a+1)
+}
+
+executadoPrimeiro { (a) in
+	print()
+	print("segunda chamada, com corpo diferente!")
+	print(a+3)
+}
+
+
