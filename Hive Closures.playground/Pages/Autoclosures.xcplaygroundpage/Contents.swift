5e60640a8e952cd1d9c45b2ad8be822b279db920
pietro1704 <pietropugliesi@icloud.com>
2019-09-09 10:55:45 -0300

    fazendo autoclosures

--- Hive Closures.playground/Pages/Autoclosures.xcplaygroundpage/Contents.swift
+++ Hive Closures.playground/Pages/Autoclosures.xcplaygroundpage/Contents.swift
@@ -3,15 +3,16 @@ import UIKit
 var lista = [1, 2, 3, 4, 5]
 
 let primeiro = lista.remove(at: 0)
-print(lista.count)
 
 //autoclosure = closure criada automaticamente
 //passada como argumento da funcao
 let primeiroClosure = {lista.remove(at: 0)}
-print(lista.count)
+print("lista antes = ", lista)
+print()
 
 //elemento não é removido até a closure ser chamada
-print(primeiroClosure())
-print(lista.count)
+print("primeiroClosure = ", primeiroClosure())
+print("lista = ", lista)
+
 
 
