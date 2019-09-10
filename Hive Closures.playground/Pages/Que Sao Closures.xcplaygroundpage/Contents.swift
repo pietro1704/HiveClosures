0c96b5457c3943212ae6178e7880562a7e635606
pietro1704 <pietropugliesi@icloud.com>
2019-09-10 12:33:06 -0300

    que sao closures 2

--- Hive Closures.playground/Pages/Que Sao Closures.xcplaygroundpage/Contents.swift
+++ Hive Closures.playground/Pages/Que Sao Closures.xcplaygroundpage/Contents.swift
@@ -7,4 +7,18 @@ let soma:(Int, Int) -> Int  = {(a, b) in
 
 soma(2, 3)
 
+//encadeamento de funcoes
 soma(soma(2, 4), soma(soma(1, 2), 3))
+
+let nome = "Aluno"
+let oi = {
+	print("olá \(nome)")
+}
+
+print("ainda nao chamei oi()")
+oi()
+let oi2 = {(name:String) in
+	print("oi \(name)")
+}
+oi2("aluno2")
+
