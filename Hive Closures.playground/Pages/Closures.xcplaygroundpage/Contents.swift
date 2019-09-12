aa21d4d86a97c7c409306a968df3fafbfdfeac0a
pietro1704 <pietropugliesi@icloud.com>
2019-09-12 10:14:23 -0300

    organizando tudo

--- Hive Closures.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Hive Closures.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -1,31 +1,40 @@
 import UIKit
 
-//passo a funcao de completion como parâmetro
+//passo a funcao de completion como parâmetro para ser usada aqui dentro
 func executadoPrimeiro(completion: (Int) -> ()){
-	print("primeiro")
+	print()
+	print("executado primeiro")
+
+	//posso usar a funcao de completion pq foi passada como parametro
 	completion(2)
 }
 
 func executadoDepois(){
-	print("Acabei")
+	print("executado depois")
 }
 
 //na chamada de executadoPrimeiro eu defino o corpo da completion
-executadoPrimeiro { (a:Int) -> () in
+executadoPrimeiro { (a) in
+// a é o parametro de completion definido em executadoPrimeiro
 
-	print("primeira chamada da closure, posso chamar a funcao executadoDepois(), olha só")
+	//posso até chamar outras funcoes
 	executadoDepois()
 
-	print(a+1)
+	print(a*3)
 }
+//rodar até aqui para testar
 
+
+//posso executar a funcao definindo completion de jeito diferente
 executadoPrimeiro { (a) in
+
+	//tenta digitar a chamada de executadoPrimeiro e aperte enter no placeholder :)
 	print("a = ", a)
 }
 
 executadoPrimeiro { (a) in
 	print()
-	print("segunda chamada, com corpo diferente!")
+	print("terceira chamada, com corpo diferente!")
 	print(a+3)
 }
 
--- Hive Closures.playground/Pages/Funcoes objetos.xcplaygroundpage/Contents.swift
+++ Hive Closures.playground/Pages/Funcoes objetos.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//funcao como conhecemos
 func soma(x:Int, y: Int) -> Int {
     return x + y
 }
@@ -10,17 +11,22 @@ func mult(x:Int, y: Int) -> Int {
 
 soma(x: 3, y: 2)
 
+//funcao como variavel
 let myFunc = soma
 
 myFunc(2,8)
 
+//funcao como parametro, usado dentro da funcao que chama
 func executeOperation(func f: (Int,Int) -> Int, over x: Int, and y: Int) -> Int {
     return f(x,y)
 }
 
 executeOperation(func: soma, over: 2, and: 1)
-executeOperation(func: mult, over: 2, and: 1)
+executeOperation(func: mult, over: 2, and: 3)
 
+
+
+//retorna funcao ((Int, Int) -> (Int)) = mult
 func myOperation() -> ( (Int,Int) -> Int ) {
     return mult
 }
@@ -29,13 +35,18 @@ let myFunc2 = myOperation()
 
 myFunc2(2,4)
 
-func multiply(_ against: Int) -> ((Int) -> Int) {
+//retorno funcao
+func multiplica(por multiplicador: Int) -> ((Int) -> Int) {
+
+	//retorna funcao (Int -> Int)
     return { (x: Int) -> Int in
-        return mult(x: x,y: against)
+
+		//retorna Int
+        return mult(x: x,y: multiplicador)
     }
 }
 
-let double = multiply(2)
+let duplica = multiplica(por: 2)
 
-double(3)
-double(8)
+duplica(3)
+duplica(8)
